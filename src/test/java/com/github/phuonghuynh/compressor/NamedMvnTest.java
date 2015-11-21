42763468260651d2d00ccbf061598a621fda74ea
Phuong H <phuonghqh@gmail.com>
2015-11-21 22:44:56 +0700

    New project

---
+++ src/test/java/com/github/phuonghuynh/compressor/NamedMvnTest.java
@@ -0,0 +1,19 @@
+package com.github.phuonghuynh.compressor;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NamedMvnTest {
+
+  @Before
+  public void before() throws IllegalAccessException, NoSuchFieldException, SecurityException {
+
+  }
+
+  @Test
+  public void testNamedMvn() throws MojoExecutionException, MojoFailureException {
+
+  }
+}
